13c518780915b4248bdc83e84d2e0c1f034356e7
Thomas Bouillon <tbouillon54@gmail.com>
2023-06-24 20:54:15 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Petit roudoudou
+
+This repo is the source code of [Petit Roudoudou](https://petit-roudoudou.fr/)
---
+++ apps/storefront/app/page.tsx
@@ -0,0 +1,5 @@
+export default async function Index() {
+  return (
+    <h1 className="text-center font-serif text-4xl">Hello world</h1>
+  );
+}
