beb136c5438848a4af2e86ec5eb678918072a03c
Thomas Bouillon <tbouillon54@gmail.com>
2023-12-01 18:59:05 +0100

    fix: admin auth guard not waiting for isAdmin query

--- apps/storefront/guards/AuthGuard.tsx
+++ apps/storefront/guards/AuthGuard.tsx
@@ -23,7 +23,7 @@ export default function AuthGuard({
   const { userQuery, isAdminQuery } = useAuth();
 
   // Pending state
-  if (userQuery.isPending || (adminOnly && isAdminQuery.isPending))
+  if (userQuery.isPending || (adminOnly && isAdminQuery.isFetching))
     return (
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <Spinner className="w-8 h-8" />
