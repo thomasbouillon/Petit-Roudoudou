73706abf52987037aee75cbad4c2b12a172377d8
Thomas Bouillon <tbouillon54@gmail.com>
2024-01-10 16:14:52 +0100

    chore: attach current cart item count to insights profile

--- apps/storefront/contexts/PostHog.tsx
+++ apps/storefront/contexts/PostHog.tsx
@@ -4,7 +4,6 @@ import { PostHogProvider as BasePostHogProvider } from 'posthog-js/react';
 import { usePathname, useSearchParams } from 'next/navigation';
 import { useEffect, useRef } from 'react';
 import env from '../env';
-import { useCart } from './CartContext';
 
 if (typeof window !== 'undefined' && env.POSTHOG_ENABLED) {
   posthog.init(env.POSTHOG_API_KEY, {
@@ -21,9 +20,6 @@ export function PostHogPageview(): JSX.Element {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
-  const prevItemsCount = useRef<number | null>(null);
-  const { getCartQuery } = useCart();
-
   useEffect(() => {
     if (pathname) {
       let url = window.origin + pathname;
@@ -34,15 +30,6 @@ export function PostHogPageview(): JSX.Element {
       const toCapture: Properties = {
         $current_url: url,
       };
-
-      // side effect, attach cart items count to profile
-      const cartItemCount = getCartQuery.data?.items.length ?? 0;
-      if (!getCartQuery.isPending && prevItemsCount.current !== cartItemCount) {
-        toCapture.$set = {
-          cartItemCount,
-        };
-        prevItemsCount.current = cartItemCount;
-      }
 
       // Capture pageview
       posthog.capture('$pageview', toCapture);
