8785cb12839d4c8ef29af7f85b9406c727648498
Thomas Bouillon <tbouillon54@gmail.com>
2024-01-15 02:05:09 +0100

    chore: force dynamic on sitemaps

--- apps/storefront/app/sitemap.ts
+++ apps/storefront/app/sitemap.ts
@@ -4,6 +4,8 @@ import env from '../env';
 import { collection, doc, getDoc } from 'firebase/firestore';
 import useDatabase from '../hooks/useDatabase';
 
+export const dynamic = 'force-dynamic';
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const db = useDatabase();
 
