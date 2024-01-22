1f13bc53775c7df7cee657e5f88a56d62d72eea1
Thomas Bouillon <tbouillon54@gmail.com>
2024-01-23 00:24:14 +0100

    fix: TS

--- libs/seo/src/lib/structuredData.ts
+++ libs/seo/src/lib/structuredData.ts
@@ -103,7 +103,7 @@ export function inStockArticle(article: Article, stockIndex: number): Product {
   return r;
 }
 
-export function organization(BASE_URL: string): Organization {
+export function organization(BASE_URL: string): Omit<Organization, string> {
   return {
     '@type': 'Organization',
     name: 'Petit Roudoudou',
