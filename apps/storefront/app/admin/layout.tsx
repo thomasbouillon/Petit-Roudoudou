0244cab9d2457a0afbd3568b1b32e69342af5747
Thomas Bouillon <tbouillon54@gmail.com>
2024-02-06 01:47:37 +0100

    fix: error boundary for devices not supporting Web Push

--- apps/storefront/app/admin/layout.tsx
+++ apps/storefront/app/admin/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next';
 import AuthGuard from '../../guards/AuthGuard';
 import { PropsWithChildren } from 'react';
 import AdminNotifications from './adminNotifications';
+import { ErrorBoundary } from 'react-error-boundary';
 
 export const metadata: Metadata = {
   robots: 'noindex,nofollow',
@@ -10,8 +11,14 @@ export const metadata: Metadata = {
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <AuthGuard adminOnly>
-      <AdminNotifications />
+      <ErrorBoundary FallbackComponent={NotificationsNotSupported}>
+        <AdminNotifications />
+      </ErrorBoundary>
       <div className="px-4 min-h-[min(35rem,100vh)] mt-8 md:mt-24">{children}</div>
     </AuthGuard>
   );
 }
+
+const NotificationsNotSupported = () => {
+  return null;
+};
