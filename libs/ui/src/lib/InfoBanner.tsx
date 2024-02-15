d2c689e976109a49b9780372229d8d1b64eb3ed3
Thomas Bouillon <tbouillon54@gmail.com>
2024-02-15 17:11:42 +0100

    fix: one line banner infos

--- libs/ui/src/lib/InfoBanner.tsx
+++ libs/ui/src/lib/InfoBanner.tsx
@@ -3,7 +3,13 @@ export default function InfoBanner({ infos }: { infos: string[] }) {
   return (
     <div className="overflow-hidden">
       <div className="min-w-[200vw] md:animate-[slide-half-left_60s_linear_infinite] animate-[slide-half-left_30s_linear_infinite] bg-primary-100 text-white font-bold py-2 flex justify-around gap-8">
-        {[0, 1].map((i) => infos.map((info) => <p key={info + i}>{info}</p>))}
+        {[0, 1].map((i) =>
+          infos.map((info) => (
+            <p className="whitespace-nowrap" key={info + i}>
+              {info}
+            </p>
+          ))
+        )}
       </div>
     </div>
   );
