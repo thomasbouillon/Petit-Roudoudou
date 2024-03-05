0309e32f658b4d8e7c1321eec5fab686cb69ad0d
Thomas Bouillon <tbouillon54@gmail.com>
2024-03-05 21:00:58 +0100

    chore: change article card aspect ratio

--- apps/storefront/app/boutique/card.tsx
+++ apps/storefront/app/boutique/card.tsx
@@ -39,19 +39,24 @@ export default function Card({
         <Image
           src={image}
           alt=""
-          className="w-full h-full object-cover aspect-[380/230]"
+          className="w-full h-full object-cover aspect-square"
           loader={loader}
-          width={384}
-          height={230}
+          width={256}
+          height={256}
           placeholder={placeholderDataUrl ? 'blur' : 'empty'}
           blurDataURL={placeholderDataUrl}
           priority={imageIsPriority}
         />
       </div>
       <div className="shadow-lg mb-4 bg-white rounded-b-md flex-grow px-4 flex flex-col">
-        <h3 className="text-2xl font-serif pt-2">
+        <h3 className="text-2xl font-serif pt-2 text-center sm:text-start text-pretty">
           {title}
-          {variant === 'customizable-article' && <span className="text-primary-100"> à personnaliser en ligne</span>}
+          {variant === 'customizable-article' && (
+            <span className="text-primary-100">
+              {' '}
+              à personnaliser <span className="max-sm:sr-only">en ligne</span>
+            </span>
+          )}
         </h3>
         {rating !== undefined && (
           <p className="absolute right-2 top-2 bg-white py-2 px-4 rounded-full flex items-center gap-1 text-sm">
@@ -60,16 +65,18 @@ export default function Card({
             <StarIcon className="w-4 h-4 text-primary-100" />
           </p>
         )}
-        <p className="mt-1 mb-3 line-clamp-6 flex-grow">{description}</p>
-        {variant === 'customizable-article' && <p className="text-primary-100">À partir de</p>}
-        <div className="flex justify-between">
+        <div className="flex-grow">
+          <p className="mt-1 line-clamp-4 max-md:sr-only">{description}</p>
+        </div>
+        <div className="flex flex-col sm:flex-row items-center sm:items-end sm:justify-between mt-3">
+          {variant === 'customizable-article' && <p className="sm:text-primary-100">À partir de</p>}
           <PrettyPrice price={price} />
           {stock !== undefined && stock > 0 && <p className="mt-auto text-primary-100">Expédition 48h</p>}
           {stock === 0 && <p className="mt-auto text-primary-100">Rupture de stock</p>}
         </div>
         <Link
           className={clsx(
-            'w-4/5 mx-auto block text-center mt-auto translate-y-4',
+            'sm:w-4/5 px-2 sm:px-8 mx-auto block text-center mt-auto translate-y-4',
             variant === 'customizable-article' && 'btn-light bg-white border-2 border-current',
             variant === 'default' && 'btn-primary'
           )}
