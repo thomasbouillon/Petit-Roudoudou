cc8a8d92e6df4add8b875f0e4eee730a816b6261
Thomas Bouillon <tbouillon54@gmail.com>
2024-03-18 20:28:37 +0100

    feat: thanks screen

--- apps/storefront/app/panier/confirmation/page.tsx
+++ apps/storefront/app/panier/confirmation/page.tsx
@@ -12,6 +12,7 @@ import { Dialog, Transition } from '@headlessui/react';
 import Link from 'next/link';
 import { routes } from '@couture-next/routing';
 import WebsiteSurvey from './survey';
+import Image from 'next/image';
 
 export default function Page() {
   const { userQuery } = useAuth();
@@ -50,97 +51,126 @@ export default function Page() {
   };
 
   return (
-    <div className="max-w-3xl mx-auto shadow-sm border rounded-sm mt-8 px-4 py-8 text-center">
-      <h1 className="font-serif text-3xl mb-4">Confirmation de paiement</h1>
-      {currentOrderQuery.data?.status === 'paid' && (
-        <>
-          <p>Votre paiement a bien été pris en compte.</p>
-          <p className="mt-2">
-            <span className="font-bold">Merci</span> pour votre commande !
-          </p>
-          <WebsiteSurvey onSubmited={goBackToHome} />
-        </>
-      )}
-      {currentOrderQuery.data?.status === 'waitingBankTransfer' && (
-        <>
-          <p>Les instructions pour effectuer votre virement vous ont été envoyées par email.</p>
-          <p className="mt-2">
-            <span className="font-bold">Merci</span> pour votre commande !
-          </p>
-          <WebsiteSurvey onSubmited={goBackToHome} />
-        </>
-      )}
-      {currentOrderQuery.data?.status === 'draft' && (
-        <>
-          <p>Votre paiement a bien été pris en compte.</p>
-          <div className="flex items-center justify-center my-8">
-            <Spinner className="w-6 h-6" />
-          </div>
-          <p>Enregistrement de votre commande...</p>
-        </>
-      )}
-      {currentOrderQuery.isPending && (
-        <>
-          <div className="flex items-center justify-center my-8">
-            <Spinner className="w-6 h-6" />
-          </div>
-          <p>Chargement de votre commande...</p>
-        </>
-      )}
-      <Transition
-        appear
-        show={
-          !warningDismissed &&
-          timeoutEnded &&
-          (currentOrderQuery.isPending || currentOrderQuery.data?.status === 'draft')
-        }
-        as={Fragment}
-      >
-        <Dialog as="div" className="relative z-10" onClose={close}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="w-full max-w-md rounded-2xl bg-white p-6 text-left shadow-xl transition-opacity space-y-2">
-                  <Dialog.Title className="font-serif text-2xl" as="h2">
-                    Oups...
-                  </Dialog.Title>
-                  <p>
-                    Il semblerai que le traitement prenne plus de temps que prévu. Nous avons reçu une alerte et nous
-                    allons vérifier votre commande manuellement.
-                  </p>
-                  <p>Vous recevrez un email dès que la commande sera validée.</p>
-                  <p>
-                    Si vous avez la moindre question, n'hésitez pas à nous contacter directement par instagram ou email.
-                  </p>
-                  <button type="button" className="btn-primary mx-auto mt-4" onClick={() => setWarningDismissed(true)}>
-                    Fermer
-                  </button>
-                </Dialog.Panel>
-              </Transition.Child>
+    <div className="pt-[20vh]">
+      <div className="max-w-3xl mx-auto shadow-sm border rounded-sm mt-8 px-4 py-8 text-center">
+        <h1 className="font-serif text-3xl mb-4">Confirmation de paiement</h1>
+        {currentOrderQuery.data?.status === 'paid' && (
+          <>
+            <p>Votre paiement a bien été pris en compte.</p>
+            <p className="mt-2">
+              <span className="font-bold">Merci</span> pour votre commande !
+            </p>
+            <WebsiteSurvey onSubmited={goBackToHome} />
+          </>
+        )}
+        {currentOrderQuery.data?.status === 'waitingBankTransfer' && (
+          <>
+            <p>Les instructions pour effectuer votre virement vous ont été envoyées par email.</p>
+            <p className="mt-2">
+              <span className="font-bold">Merci</span> pour votre commande !
+            </p>
+            <WebsiteSurvey onSubmited={goBackToHome} />
+          </>
+        )}
+        {currentOrderQuery.data?.status === 'draft' && (
+          <>
+            <p>Votre paiement a bien été pris en compte.</p>
+            <div className="flex items-center justify-center my-8">
+              <Spinner className="w-6 h-6" />
             </div>
-          </div>
-        </Dialog>
-      </Transition>
+            <p>Enregistrement de votre commande...</p>
+          </>
+        )}
+        {currentOrderQuery.isPending && (
+          <>
+            <div className="flex items-center justify-center my-8">
+              <Spinner className="w-6 h-6" />
+            </div>
+            <p>Chargement de votre commande...</p>
+          </>
+        )}
+        <Transition
+          appear
+          show={
+            !warningDismissed &&
+            timeoutEnded &&
+            (currentOrderQuery.isPending || currentOrderQuery.data?.status === 'draft')
+          }
+          as={Fragment}
+        >
+          <Dialog as="div" className="relative z-10" onClose={close}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-black bg-opacity-25" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 overflow-y-auto">
+              <div className="flex min-h-full items-center justify-center p-4 text-center">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
+                >
+                  <Dialog.Panel className="w-full max-w-md rounded-2xl bg-white p-6 text-left shadow-xl transition-opacity space-y-2">
+                    <Dialog.Title className="font-serif text-2xl" as="h2">
+                      Oups...
+                    </Dialog.Title>
+                    <p>
+                      Il semblerai que le traitement prenne plus de temps que prévu. Nous avons reçu une alerte et nous
+                      allons vérifier votre commande manuellement.
+                    </p>
+                    <p>Vous recevrez un email dès que la commande sera validée.</p>
+                    <p>
+                      Si vous avez la moindre question, n'hésitez pas à nous contacter directement par instagram ou
+                      email.
+                    </p>
+                    <button
+                      type="button"
+                      className="btn-primary mx-auto mt-4"
+                      onClick={() => setWarningDismissed(true)}
+                    >
+                      Fermer
+                    </button>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </Dialog>
+        </Transition>
+      </div>
+      {currentOrderQuery.data?.status === 'paid' || currentOrderQuery.data?.status === 'waitingBankTransfer' ? (
+        <ThanksAnimation />
+      ) : (
+        <ThanksAnimationPlaceholder />
+      )}
     </div>
   );
 }
+
+const ThanksAnimation = () => {
+  return (
+    <div className="grid grid-cols-2 gap-[50vw]">
+      <video src="/videos/thanks.mp4" autoPlay playsInline loop className="aspect-square" />
+      <video src="/videos/thanks.mp4" autoPlay playsInline loop className="aspect-square scale-x-[-1]" />
+    </div>
+  );
+};
+
+const ThanksAnimationPlaceholder = () => {
+  return (
+    <div className="grid grid-cols-2 gap-[50vw]">
+      <div className="aspect-square"></div>
+    </div>
+  );
+};
