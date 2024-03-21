448a1e808f76f81151d345a1847c04018c65942f
Thomas Bouillon <tbouillon54@gmail.com>
2024-03-21 22:39:18 +0100

    fix: cart preview css

--- libs/ui/src/lib/form/QuantityWidget.tsx
+++ libs/ui/src/lib/form/QuantityWidget.tsx
@@ -21,7 +21,7 @@ export function QuantityWidget({ value, onChange, disableMinus, disablePlus }: P
       <button
         type="button"
         className={clsx(
-          'mt-[-2px] scale-150 origin-center text-center absolute top-1/2 left-0 -translate-y-1/2 pl-3 pr-1 focus:outline-none',
+          'mt-[-2px] scale-150 origin-center text-center absolute top-1/2 left-0 -translate-y-1/2 translate-x-2 px-2 focus:outline-none',
           disableMinus && 'opacity-30 cursor-not-allowed'
         )}
         onClick={() => onChange(value - 1)}
@@ -32,7 +32,7 @@ export function QuantityWidget({ value, onChange, disableMinus, disablePlus }: P
       <button
         type="button"
         className={clsx(
-          'mt-[-2px] scale-150 origin-center text-center absolute top-1/2 right-0 -translate-y-1/2 pl-1 pr-3 focus:outline-none',
+          'mt-[-2px] scale-150 origin-center text-center absolute top-1/2 right-0 -translate-y-1/2 -translate-x-2 px-2 focus:outline-none',
           disablePlus && 'opacity-30 cursor-not-allowed'
         )}
         onClick={() => onChange(value + 1)}
