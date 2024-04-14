97504f9a05870682f8d981f59ba9d1d8cfc23eae
Thomas Bouillon <tbouillon54@gmail.com>
2024-04-14 14:46:04 +0200

    fix: cicd

--- libs/api-connector/src/lib/router.ts
+++ libs/api-connector/src/lib/router.ts
@@ -1,11 +1,9 @@
 import fabricGroups from './routers/fabric-groups';
 import fabricTags from './routers/fabric-tags';
 import fabrics from './routers/fabrics';
-import { helloWorldRouter } from './routers/hello-world';
 import { router } from './trpc';
 
 export const trpcRouter = router({
-  hello: helloWorldRouter,
   fabrics,
   fabricGroups,
   fabricTags,
