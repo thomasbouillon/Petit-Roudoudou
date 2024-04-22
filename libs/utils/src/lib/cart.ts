0156eb308e64b932c00d06ceaf96e18bd9a3c1df
Thomas Bouillon <tbouillon54@gmail.com>
2024-04-22 21:48:04 +0200

    feat: show shipping offered before payment

--- libs/utils/src/lib/cart.ts
+++ libs/utils/src/lib/cart.ts
@@ -3,11 +3,3 @@ import { Cart } from '@prisma/client';
 export function cartContainsCustomizedItems(cart: Cart): boolean {
   return cart.items.some((item) => item.type === 'customized');
 }
-
-export function cartTotalTaxIncludedWithOutGiftCards(cart: Cart | null): number {
-  if (!cart) return 0;
-  return (
-    cart.totalTaxIncluded -
-    cart.items.filter((item) => item.type === 'giftCard').reduce((total, item) => total + item.totalTaxIncluded, 0)
-  );
-}
