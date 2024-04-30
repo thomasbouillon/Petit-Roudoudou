04d290a4e862faac9c39b8d10c601836419fa178
odinalx <odin.alexandre26@gmail.com>
2024-04-30 11:22:15 +0200

    feat: add front for filter in shop

--- apps/storefront/app/boutique/ArticlesNavigationPopover.tsx
+++ apps/storefront/app/boutique/ArticlesNavigationPopover.tsx
@@ -2,8 +2,8 @@
 
 import { routes } from '@couture-next/routing';
 import { Article } from '@couture-next/types';
-import { Popover, Transition } from '@headlessui/react';
-import { ArrowTopRightOnSquareIcon, XMarkIcon } from '@heroicons/react/24/solid';
+import { Popover, Transition, Disclosure } from '@headlessui/react';
+import { ArrowTopRightOnSquareIcon, XMarkIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
 import Link from 'next/link';
 import { Fragment } from 'react';
 
@@ -25,28 +25,52 @@ export function ArticlesNavigationPopover({ articles }: Props) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Popover.Panel className="absolute z-20 left-1/2 -translate-x-1/2 translate-y-4 w-full max-w-prose p-4">
+        <Popover.Panel className="absolute z-20 left-1/2 -translate-x-1/2 translate-y-4 w-full sm:w-1/4  max-w-prose p-4">
           {({ close }) => (
-            <div className="relative bg-white p-6 border rounded shadow-md">
-              <div className="grid grid-cols-[1fr_auto] gap-2">
-                <h2 className="text-lg underline">Toutes les catégories:</h2>
+            <div className="relative bg-white  border rounded shadow-md">
+              <div className="grid grid-cols-[1fr_auto] gap-2 p-6 border-b border-black">
+                <h2 className="text-lg underline">Trier</h2>
                 <button type="button" onClick={() => close()} aria-label="Fermer la popup">
                   <XMarkIcon className="w-6 h-6" />
                 </button>
               </div>
 
-              <nav aria-label="Navigation parmis les articles" className=" mt-4">
-                <ul className="empty:hidden grid grid-cols-[repeat(auto-fill,minmax(28ch,1fr))] items-center gap-2">
-                  {articles.map((article) => (
-                    <li className="relative pr-5 !outline-none border rounded-full" key={article.id}>
-                      <Link href={routes().shop().article(article.slug).index()} className="w-full block px-4 py-2">
-                        {article.namePlural}
-                        <ArrowTopRightOnSquareIcon className="inline-block w-5 h-5 absolute right-2 top-1/2 -translate-y-1/2" />
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
+              <nav aria-label="Navigation parmis les articles" className=" overflow-hidden">
+                <Disclosure>
+                  <Disclosure.Button className="border-b border-black p-4 w-full z-10 grid grid-cols-[1fr_auto] !outline-none ">
+                    <span className="empty:block ">Catégorie d'article</span>
+                    <span>
+                      <ChevronRightIcon className="w-6 h-6 ml-4 ui-not-open:transform-none ui-open:rotate-90 ui-open:transform"></ChevronRightIcon>
+                    </span>
+                  </Disclosure.Button>
+
+                  <Transition
+                    className="relative transition-transform ease-out"
+                    enterFrom="-translate-y-1/4"
+                    enterTo="translate-y-0"
+                    leaveFrom="translate-y-0"
+                    leaveTo="-translate-y-1/4"
+                  >
+                    <>
+                      <Disclosure.Panel className="flex flex-col gap-4 !outline-none max-w-prose mx-auto">
+                        <ul className="empty:hidden flex flex-col p-4 ui-not-open:border-0 border-b gap-2 border-black">
+                          {articles.map((article) => (
+                            <li className="relative !outline-none" key={article.id}>
+                              <label className="space-y-2 grid grid-cols-[1fr_auto]">
+                                {article.namePlural}
+                                <input className="ml-4" type="checkbox" />
+                              </label>
+                            </li>
+                          ))}
+                        </ul>
+                      </Disclosure.Panel>
+                    </>
+                  </Transition>
+                </Disclosure>
               </nav>
+              <div className="py-6">
+                <button className="btn-secondary mx-auto">Afficher le résultat</button>
+              </div>
             </div>
           )}
         </Popover.Panel>
