2dcbff95e930e7b3a20c4bbfdfb4acf117a17ab4
Thomas Bouillon <tbouillon54@gmail.com>
2024-05-07 22:13:12 +0200

    chore: hide delivered orders in admin

--- libs/api-connector/src/lib/routers/orders/find.ts
+++ libs/api-connector/src/lib/routers/orders/find.ts
@@ -1,18 +1,39 @@
+import { z } from 'zod';
 import { isAdmin } from '../../middlewares/isAdmin';
 import { publicProcedure } from '../../trpc';
+import { Prisma } from '@prisma/client';
 
-export default publicProcedure.use(isAdmin()).query(async ({ ctx }) => {
-  const orders = await ctx.orm.order.findMany({
-    where: {
+export default publicProcedure
+  .use(isAdmin())
+  .input(
+    z.object({
+      includeAlreadyShipped: z.boolean().default(false),
+      includeAchived: z.boolean().default(false),
+    })
+  )
+  .query(async ({ input, ctx }) => {
+    const filters: Prisma.OrderFindManyArgs['where'] = {
       status: {
         not: 'DRAFT',
       },
-      archivedAt: null,
-    },
-    orderBy: {
-      createdAt: 'desc',
-    },
-  });
+    };
 
-  return orders;
-});
+    if (!input.includeAlreadyShipped) {
+      filters.workflowStep = {
+        not: 'DELIVERED',
+      };
+    }
+
+    if (!input.includeAchived) {
+      filters.archivedAt = null;
+    }
+
+    const orders = await ctx.orm.order.findMany({
+      where: filters,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    return orders;
+  });
