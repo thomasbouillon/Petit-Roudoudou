e9248fce58fc77a101e5febe110a9fa210694785
odinalx <odin.alexandre26@gmail.com>
2024-05-14 10:00:22 +0200

    refactor: add stylised footer for larger display

--- apps/storefront/app/footer.tsx
+++ apps/storefront/app/footer.tsx
@@ -27,32 +27,113 @@ export default function Footer() {
         </Link>
         <div className="sm:triangle-top bg-light-100 absolute w-full bottom-0" />
       </div>
-      <div className="flex flex-col items-center bg-light-100 pt-8 sm:pt-0">
-        <Nav
-          items={[
-            { label: 'Accueil', href: routes().index() },
-            { label: 'Boutique', href: routes().shop().index() },
-            { label: 'Tissus', href: routes().fabrics().index() },
-            { label: 'Blog', href: routes().blog().index() },
-            { label: 'Evènements', href: routes().events().index() },
-            { label: 'Contact', href: routes().contactUs() },
-            {
-              label: 'Foire aux questions',
-              href: routes().faq().index(),
-            },
-            { label: 'Mentions légales', href: routes().legal().noticies() },
-            {
-              label: "Conditions générales d'utilisation",
-              href: routes().legal().cgu(),
-            },
-            {
-              label: 'Conditions générales de vente',
-              href: routes().legal().cgv(),
-            },
-          ]}
-          Link={Link}
-          className="mt-5"
-        />
+      <div>
+        <div className="flex flex-col items-center bg-light-100 pt-8 sm:pt-0 sm:hidden ">
+          <Nav
+            items={[
+              { label: 'Accueil', href: routes().index() },
+              { label: 'Boutique', href: routes().shop().index() },
+              { label: 'Tissus', href: routes().fabrics().index() },
+              { label: 'Blog', href: routes().blog().index() },
+              { label: 'Evènements', href: routes().events().index() },
+              { label: 'Contact', href: routes().contactUs() },
+              {
+                label: 'Foire aux questions',
+                href: routes().faq().index(),
+              },
+              { label: 'Mentions légales', href: routes().legal().noticies() },
+              {
+                label: "Conditions générales d'utilisation",
+                href: routes().legal().cgu(),
+              },
+              {
+                label: 'Conditions générales de vente',
+                href: routes().legal().cgv(),
+              },
+            ]}
+            Link={Link}
+            className="mt-5"
+          />
+        </div>
+        <div className="flex flex-col items-center gap-6 bg-light-100">
+          <div>
+            <p className="text-center font-semibold p-2">Nous Suivre</p>
+            <ul className="flex flex-row gap-4">
+              <li>
+                {' '}
+                <Link href={routes().index()}>Facebook</Link>{' '}
+              </li>
+              <li>
+                {' '}
+                <Link href={routes().index()}>Instagram</Link>{' '}
+              </li>
+              <li>
+                {' '}
+                <Link href={routes().index()}>Facebook</Link>{' '}
+              </li>
+            </ul>
+          </div>
+          <div className="flex flex-row gap-12">
+            <div>
+              <h3 className=" font-semibold">Navigation</h3>
+              <ul>
+                <li>
+                  {' '}
+                  <Link href={routes().index()}> Accueil</Link>
+                </li>
+                <li>
+                  {' '}
+                  <Link href={routes().shop().index()}> Boutique</Link>
+                </li>
+                <li>
+                  {' '}
+                  <Link href={routes().fabrics().index()}> Tissus</Link>
+                </li>
+                <li>
+                  {' '}
+                  <Link href={routes().blog().index()}>Blog</Link>{' '}
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h3 className=" font-semibold">Information</h3>
+              <ul>
+                <li>
+                  {' '}
+                  <Link href={routes().events().index()}>Evènements</Link>{' '}
+                </li>
+                <li>
+                  {' '}
+                  <Link href={routes().contactUs()}>Contact</Link>{' '}
+                </li>
+                <li>
+                  {' '}
+                  <Link href={routes().faq().index()}>Foire aux questions</Link>{' '}
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h3 className=" font-semibold">Loi et l'ordre</h3>
+              <ul>
+                <li>
+                  {' '}
+                  <Link href={routes().legal().cgu()}>Conditions générales d'utilisation</Link>{' '}
+                </li>
+                <li>
+                  {' '}
+                  <Link href={routes().legal().cgv()}>Conditions générales de vente</Link>{' '}
+                </li>
+                <li>
+                  {' '}
+                  <Link href={routes().legal().noticies()}>Mentions légales</Link>{' '}
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="p-2 text-gray-600">
+            <p>© 2024 Petit Roudoudou Tous droits réservés</p>
+          </div>
+        </div>
       </div>
     </footer>
   );
