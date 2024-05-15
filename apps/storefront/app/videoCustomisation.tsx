7a66768fefddfd5887c2eff5980f20ebbec939af
odinalx <odin.alexandre26@gmail.com>
2024-05-15 13:39:39 +0200

    refactor: add better video and change length of the text

--- apps/storefront/app/videoCustomisation.tsx
+++ apps/storefront/app/videoCustomisation.tsx
@@ -5,16 +5,13 @@ export async function VideoCustomisation() {
   return (
     <div className="flex flex-col justify-center items-center gap-4 my-10 ">
       <h2 className="font-serif text-center text-3xl">Créez l’univers de votre enfant en quelques clics !</h2>
-      <div className="flex md:flex-row flex-col items-center justify-center gap-4 md:max-w-[44.25rem] ">
+      <div className="flex md:flex-row flex-col items-center justify-center gap-4 md:max-w-[35.25rem] ">
         <video id="customizationVideo" className="  max-w-[256px] " controls autoPlay muted loop>
           <source src="/videos/CustomArticles.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <div className="flex flex-col gap-6 break-all px-6">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation{' '}
-          </p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
           <Link
             href={routes().shop().index({ customizableOnly: true })}
             className="btn-secondary text-center px-7 py-3 text-lg bg-white"
