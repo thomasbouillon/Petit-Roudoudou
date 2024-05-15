5899f94b90ef1e35c53166f24595bdb3264b7c5c
odinalx <odin.alexandre26@gmail.com>
2024-05-15 14:21:56 +0200

    refactor: add webm foramt for the video

--- apps/storefront/app/videoCustomisation.tsx
+++ apps/storefront/app/videoCustomisation.tsx
@@ -8,6 +8,7 @@ export async function VideoCustomisation() {
       <div className="flex md:flex-row flex-col items-center justify-center gap-4 md:max-w-[35.25rem] ">
         <video id="customizationVideo" className="  max-w-[256px] " autoPlay muted loop playsInline>
           <source src="/videos/CustomArticles.mp4" type="video/mp4" />
+          <source src="/videos/CustomArticles.webm" type="video/webm" />
           Your browser does not support the video tag.
         </video>
         <div className="flex flex-col gap-6 break-all px-6">
