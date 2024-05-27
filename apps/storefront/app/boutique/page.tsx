fbff2326234fa9553758607f6f7f1d6bec918e07
Thomas Bouillon <tbouillon54@gmail.com>
2024-05-27 17:44:33 +0200

    fix: responsive

--- apps/storefront/app/boutique/page.tsx
+++ apps/storefront/app/boutique/page.tsx
@@ -22,15 +22,15 @@ export default async function Page() {
       <div className="flex flex-col-reverse">
         <h1 className="font-serif text-3xl text-center mb-6 mt-8">Toutes les créations</h1>
         <div className="mt-8 space-y-8">
-          <div className="relative flex justify-center items-center bg-light-100 border-b border-light-200 py-2">
+          <div className="relative flex flex-col lg:flex-row gap-4 justify-center items-center bg-light-100 shadow py-2">
             <StorageImage
               alt="Un exemple de carte cadeau petit roudoudou"
               src="public/images/gift-card1.png"
-              className="mx-2 border border-black rounded-lg"
-              width={200}
-              height={85}
+              className="rounded-lg object-contain h-32"
+              width={256}
+              height={128}
             />
-            <span className="text-xs sm:text-sm px-2  leading-5 aria-hidden:">
+            <span className="text-xs sm:text-sm px-2 leading-5 aria-hidden:">
               <p>Découvrez nos cartes cadeaux virtuelles </p>
               <p>pour offrir à vos proches ou collègues !</p>
               <p className="underline font-semibold text-center mt-2">Découvrir</p>
@@ -38,9 +38,9 @@ export default async function Page() {
             <StorageImage
               alt="Un exemple de carte cadeau petit roudoudou"
               src="public/images/gift-card2.png"
-              className="mx-2 border border-black rounded-lg"
-              width={200}
-              height={85}
+              className="rounded-lg h-32 object-contain hidden lg:block"
+              width={256}
+              height={128}
             />
             <Link className="absolute top-0 left-0 right-0 bottom-0 " href={routes().shop().createGiftCard()}>
               <span className="sr-only">Création de carte cadeau</span>
