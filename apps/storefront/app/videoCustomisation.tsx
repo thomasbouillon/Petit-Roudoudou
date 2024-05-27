46c2d69f07766c28ec32ec798a59415fb6ad3045
Thomas Bouillon <tbouillon54@gmail.com>
2024-05-27 21:22:56 +0200

    chore: move videos to CDN

--- apps/storefront/app/videoCustomisation.tsx
+++ apps/storefront/app/videoCustomisation.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link';
 import { routes } from '@couture-next/routing';
-import { StyledWrapper, WithDecorativeDotsWrapper } from '@couture-next/ui';
+import { WithDecorativeDotsWrapper } from '@couture-next/ui';
+import env from '../env';
+
+const getVideoUrl = (pathname: string) => new URL(pathname, env.CDN_BASE_URL).toString();
 
 export async function VideoCustomisation() {
   return (
@@ -8,10 +11,11 @@ export async function VideoCustomisation() {
       <h2 className="sr-only">Toutes nos créations sont 100% personnalisables sur le site.</h2>
       <div className="flex justify-center">
         <div className="flex md:flex-row flex-col items-center justify-center gap-4 md:max-w-[35.25rem] ">
-          <video id="customizationVideo" className="max-w-[256px] shadow rounded-sm" autoPlay muted loop playsInline>
-            <source src="/videos/CustomArticles.mp4" type="video/mp4" />
-            <source src="/videos/CustomArticles.webm" type="video/webm" />
-            Your browser does not support the video tag.
+          <video className="max-w-[256px] shadow rounded-sm" autoPlay muted loop playsInline>
+            <source src={getVideoUrl('public/videos/CustomArticles.mp4')} type="video/mp4" />
+            <source src={getVideoUrl('public/videos/CustomArticles.webm')} type="video/webm" />
+            Video de présentation de plusieurs articles avec différents tissus dans notre outil de personnalisation en
+            ligne
           </video>
           <div className="flex flex-col gap-6 px-6">
             <p className="font-bold">Créations 100% uniques avec le système de personnalisation en ligne</p>
