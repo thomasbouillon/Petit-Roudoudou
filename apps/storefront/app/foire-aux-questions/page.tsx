005e763b4174dc1cdf3159fc60753db728d18433
odinalx <odin.alexandre26@gmail.com>
2024-05-28 11:10:04 +0200

    refactor: rename faq into aboutUs link not modified yet

--- apps/storefront/app/foire-aux-questions/page.tsx
+++ apps/storefront/app/foire-aux-questions/page.tsx
@@ -4,18 +4,18 @@ import { routes } from '@couture-next/routing';
 import { generateMetadata } from '@couture-next/utils';
 
 export const metadata = generateMetadata({
-  title: 'Foire aux questions',
+  title: 'Qui sommes-nous ?',
   alternates: { canonical: routes().faq().index() },
   description:
-    'Vous avez des questions ? Retrouvez les réponses aux questions les plus fréquentes sur Petit Roudoudou.',
+    'Vous avez des questions à propos de nous ? Retrouvez les réponses aux questions les plus fréquentes sur Petit Roudoudou.',
 });
 
 export default function Page() {
   return (
     <div className="max-3xl mx-auto px-4">
-      <h1 className="font-serif text-center text-3xl mt-8">Foire aux questions</h1>
+      <h1 className="font-serif text-center text-3xl mt-8">Qui sommes-nous ?</h1>
       <p className="text-center mt-4">
-        Tu ne trouves pas la réponse à ta question ? RDV sur notre{' '}
+        Si tu as besoin d'aide ou que tu ne trouves pas de réponse à ta question ? RDV sur notre{' '}
         <Link className="underline" href={routes().contactUs()}>
           page de contact
         </Link>
