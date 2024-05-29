ef417e7b7a3d710c28b6ddf51aff466cf1bc91ef
Thomas Bouillon <tbouillon54@gmail.com>
2024-05-29 10:22:25 +0200

    fix: article full desc css

--- apps/storefront/app/boutique/[articleSlug]/[inStockSlug]/ArticleDescription.tsx
+++ apps/storefront/app/boutique/[articleSlug]/[inStockSlug]/ArticleDescription.tsx
@@ -8,8 +8,10 @@ type Props = {
 export default function ArticleDescritpion({ article, stockIndex }: Props) {
   const stock = article.stocks[stockIndex];
 
+  if (!stock.fullDescription) return null;
+
   return (
-    <section className="flex flex-col items-center mt-16 mb-8" id="article-fullDesscription">
+    <section className="flex flex-col items-center mt-16 mb-8 max-w-prose mx-auto">
       <h2 className="text-2xl font-serif mb-4">Description</h2>
       <div className="px-4 mx-4">{stock.fullDescription}</div>
     </section>
