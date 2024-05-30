f5ec061e71192a70d3287088e9774c0307599379
odinalx <odin.alexandre26@gmail.com>
2024-05-30 10:36:31 +0200

    feature: add stocks of the good category images when selecting one

--- apps/storefront/app/admin/seo/seoFields.tsx
+++ apps/storefront/app/admin/seo/seoFields.tsx
@@ -6,18 +6,33 @@ import { loader } from '../../../utils/next-image-firebase-storage-loader';
 import { Field } from '@couture-next/ui';
 import { Disclosure } from '@headlessui/react';
 import { RadioGroup } from '@headlessui/react';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
+import { Article } from '@couture-next/types';
 import { ChevronRightIcon } from '@heroicons/react/24/outline';
 
 //----------------------------------------------------------------------
 
 //----------------------------------------------------------------------
 
-export function Form() {
-  const [category, setCategory] = useState('');
+export function Form({ stockUidBlacklist }: { stockUidBlacklist?: string[] }) {
   const { data: articles, error } = trpc.articles.list.useQuery();
-  if (error) throw error;
+
+  const [category, setCategory] = useState('');
+  const [stock, setStock] = useState('');
   if (articles === undefined) return <div>Loading...</div>;
+
+  const selectedArticle = useMemo(() => articles.find((article) => article.id === category), [articles, category]);
+  const filteredStocks = useMemo(
+    () =>
+      selectedArticle
+        ? stockUidBlacklist
+          ? selectedArticle.stocks.filter((stock) => !stockUidBlacklist.includes(stock.uid))
+          : selectedArticle.stocks
+        : [],
+    [selectedArticle, stockUidBlacklist]
+  );
+
+  if (error) throw error;
   const SubmitButton = <button type="submit"></button>;
 
   return (
@@ -79,15 +94,66 @@ export function Form() {
                     )}
                   />
                 </fieldset>
-                <div>
-                  <h3 className="text-xl  p-4 py-6 font-bold">Stock</h3>
-                  <div></div>
-                  <div></div>
-                </div>
-                <div>
-                  <h3 className="text-xl p-4 py-6  font-bold">Personnaliser</h3>
-                  <div></div>
-                </div>
+                <Disclosure>
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button className="text-2xl p-4 py-6 font-bold w-full text-start">
+                        <span className="flex">
+                          {' '}
+                          Stock <ChevronRightIcon className={open ? 'rotate-90 transform max-w-8' : 'max-w-8'} />
+                        </span>
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="">
+                        <RadioGroup
+                          value={stock}
+                          onChange={setStock}
+                          className="w-11/12 mx-auto mt-2 grid grid-cols-4 gap-4 text-center"
+                        >
+                          {filteredStocks.map((stock) => (
+                            <RadioGroup.Option key={stock.uid} value={stock.uid}>
+                              {({ checked }) => (
+                                <>
+                                  <div className={checked ? 'outline-4  outline-red-600 outline' : ''}>
+                                    <Image
+                                      src={stock.images[0].url}
+                                      alt={stock.title}
+                                      className="w-full aspect-square object-cover"
+                                      loader={loader}
+                                      width={544 / 2}
+                                      height={544 / 2}
+                                    />
+                                  </div>
+                                  <span className="">{stock.title}</span>
+                                </>
+                              )}
+                            </RadioGroup.Option>
+                          ))}
+                        </RadioGroup>
+                        <fieldset className="grid grid-cols-[auto_1fr] gap-4">
+                          <p className="col-span-2 text-gray-500 text-xs text-center">
+                            Informations visibles dans le moteur de recherche Google et pour l&apos;indexation des pages
+                          </p>
+                          <Field
+                            labelClassName="min-w-[min(30vw,15rem)]"
+                            label="Titre de la page"
+                            widgetId="seo.title"
+                            renderWidget={(className) => <input type="text" id="seo.title" className={className} />}
+                          />
+                          <Field
+                            label="Description"
+                            widgetId="seo.description"
+                            helpText="Environ 160 caractères"
+                            renderWidget={(className) => (
+                              <>
+                                <textarea id="seo.description" className={className} />
+                              </>
+                            )}
+                          />
+                        </fieldset>
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
               </Disclosure.Panel>
             </>
           )}
