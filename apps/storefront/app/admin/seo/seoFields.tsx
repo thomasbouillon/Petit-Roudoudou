a27f5dfbe7a9e54131e7b7e759cf3bb29330ef48
odinalx <odin.alexandre26@gmail.com>
2024-05-30 15:20:07 +0200

    feature: add personnaliser block

--- apps/storefront/app/admin/seo/seoFields.tsx
+++ apps/storefront/app/admin/seo/seoFields.tsx
@@ -35,7 +35,7 @@ export function Form({ stockUidBlacklist }: { stockUidBlacklist?: string[] }) {
   const SubmitButton = <button type="submit"></button>;
 
   return (
-    <form className="max-w-3xl mx-auto mt-8 shadow-sm bg-white rounded-md px-4 border">
+    <form className="max-w-3xl mx-auto my-8 shadow-sm bg-white rounded-md px-4 border">
       <div>
         <Disclosure>
           {({ open }) => (
@@ -73,98 +73,137 @@ export function Form({ stockUidBlacklist }: { stockUidBlacklist?: string[] }) {
                   ))}
                 </RadioGroup>
                 {category && category.length > 0 && (
-                  <fieldset className="grid grid-cols-[auto_1fr] gap-4">
-                    <p className="col-span-2 text-gray-500 text-xs text-center">
-                      Informations visibles dans le moteur de recherche Google et pour l&apos;indexation des pages
-                    </p>
-                    <Field
-                      labelClassName="min-w-[min(30vw,15rem)]"
-                      label="Titre de la page"
-                      widgetId="seo.title"
-                      renderWidget={(className) => <input type="text" id="seo.title" className={className} />}
-                    />
-                    <Field
-                      label="Description"
-                      widgetId="seo.description"
-                      helpText="Environ 160 caractères"
-                      renderWidget={(className) => (
+                  <>
+                    <fieldset className="grid grid-cols-[auto_1fr] gap-4">
+                      <p className="col-span-2 text-gray-500 text-xs text-center">
+                        Informations visibles dans le moteur de recherche Google et pour l&apos;indexation des pages
+                      </p>
+                      <Field
+                        labelClassName="min-w-[min(30vw,15rem)]"
+                        label="Titre de la page"
+                        widgetId="seo.title"
+                        renderWidget={(className) => <input type="text" id="seo.title" className={className} />}
+                      />
+                      <Field
+                        label="Description"
+                        widgetId="seo.description"
+                        helpText="Environ 160 caractères"
+                        renderWidget={(className) => (
+                          <>
+                            <textarea id="seo.description" className={className} />
+                          </>
+                        )}
+                      />
+                    </fieldset>
+                    <Disclosure>
+                      {({ open }) => (
                         <>
-                          <textarea id="seo.description" className={className} />
+                          <Disclosure.Button className="text-2xl p-4 py-6 font-bold w-full text-start">
+                            <span className="flex">
+                              {' '}
+                              Personnaliser
+                              <ChevronRightIcon className={open ? 'rotate-90 transform max-w-8' : 'max-w-8'} />
+                            </span>
+                          </Disclosure.Button>
+                          <Disclosure.Panel className="">
+                            <fieldset className="grid grid-cols-[auto_1fr] gap-4">
+                              <p className="col-span-2 text-gray-500 text-xs text-center">
+                                Informations visibles dans le moteur de recherche Google et pour l&apos;indexation des
+                                pages
+                              </p>
+                              <Field
+                                labelClassName="min-w-[min(30vw,15rem)]"
+                                label="Titre de la page"
+                                widgetId="seo.title"
+                                renderWidget={(className) => <input type="text" id="seo.title" className={className} />}
+                              />
+                              <Field
+                                label="Description"
+                                widgetId="seo.description"
+                                helpText="Environ 160 caractères"
+                                renderWidget={(className) => (
+                                  <>
+                                    <textarea id="seo.description" className={className} />
+                                  </>
+                                )}
+                              />
+                            </fieldset>
+                          </Disclosure.Panel>
                         </>
                       )}
-                    />
-                  </fieldset>
+                    </Disclosure>
+                    <Disclosure>
+                      {({ open }) => (
+                        <>
+                          <Disclosure.Button className="text-2xl p-4 py-6 font-bold w-full text-start">
+                            <span className="flex">
+                              {' '}
+                              Stock <ChevronRightIcon className={open ? 'rotate-90 transform max-w-8' : 'max-w-8'} />
+                            </span>
+                          </Disclosure.Button>
+                          <Disclosure.Panel className="">
+                            <RadioGroup
+                              value={stock}
+                              onChange={setStock}
+                              className="w-11/12 mx-auto mt-2 grid grid-cols-4 gap-4 text-center"
+                            >
+                              {filteredStocks.map((stock) => (
+                                <RadioGroup.Option key={stock.uid} value={stock.uid}>
+                                  {({ checked }) => (
+                                    <>
+                                      <div className={checked ? 'outline-4  outline-red-600 outline' : ''}>
+                                        <Image
+                                          src={stock.images[0].url}
+                                          alt={stock.title}
+                                          className="w-full aspect-square object-cover"
+                                          loader={loader}
+                                          width={544 / 2}
+                                          height={544 / 2}
+                                        />
+                                      </div>
+                                      <span className="">{stock.title}</span>
+                                    </>
+                                  )}
+                                </RadioGroup.Option>
+                              ))}
+                            </RadioGroup>
+                            {stock && stock.length > 0 && (
+                              <fieldset className="grid grid-cols-[auto_1fr] gap-4">
+                                <p className="col-span-2 text-gray-500 text-xs text-center">
+                                  Informations visibles dans le moteur de recherche Google et pour l&apos;indexation des
+                                  pages
+                                </p>
+                                <Field
+                                  labelClassName="min-w-[min(30vw,15rem)]"
+                                  label="Titre de la page"
+                                  widgetId="seo.title"
+                                  renderWidget={(className) => (
+                                    <input type="text" id="seo.title" className={className} />
+                                  )}
+                                />
+                                <Field
+                                  label="Description"
+                                  widgetId="seo.description"
+                                  helpText="Environ 160 caractères"
+                                  renderWidget={(className) => (
+                                    <>
+                                      <textarea id="seo.description" className={className} />
+                                    </>
+                                  )}
+                                />
+                              </fieldset>
+                            )}
+                          </Disclosure.Panel>
+                        </>
+                      )}
+                    </Disclosure>
+                  </>
                 )}
-                <Disclosure>
-                  {({ open }) => (
-                    <>
-                      <Disclosure.Button className="text-2xl p-4 py-6 font-bold w-full text-start">
-                        <span className="flex">
-                          {' '}
-                          Stock <ChevronRightIcon className={open ? 'rotate-90 transform max-w-8' : 'max-w-8'} />
-                        </span>
-                      </Disclosure.Button>
-                      <Disclosure.Panel className="">
-                        <RadioGroup
-                          value={stock}
-                          onChange={setStock}
-                          className="w-11/12 mx-auto mt-2 grid grid-cols-4 gap-4 text-center"
-                        >
-                          {filteredStocks.map((stock) => (
-                            <RadioGroup.Option key={stock.uid} value={stock.uid}>
-                              {({ checked }) => (
-                                <>
-                                  <div className={checked ? 'outline-4  outline-red-600 outline' : ''}>
-                                    <Image
-                                      src={stock.images[0].url}
-                                      alt={stock.title}
-                                      className="w-full aspect-square object-cover"
-                                      loader={loader}
-                                      width={544 / 2}
-                                      height={544 / 2}
-                                    />
-                                  </div>
-                                  <span className="">{stock.title}</span>
-                                </>
-                              )}
-                            </RadioGroup.Option>
-                          ))}
-                        </RadioGroup>
-                        {stock && stock.length > 0 && (
-                          <fieldset className="grid grid-cols-[auto_1fr] gap-4">
-                            <p className="col-span-2 text-gray-500 text-xs text-center">
-                              Informations visibles dans le moteur de recherche Google et pour l&apos;indexation des
-                              pages
-                            </p>
-                            <Field
-                              labelClassName="min-w-[min(30vw,15rem)]"
-                              label="Titre de la page"
-                              widgetId="seo.title"
-                              renderWidget={(className) => <input type="text" id="seo.title" className={className} />}
-                            />
-                            <Field
-                              label="Description"
-                              widgetId="seo.description"
-                              helpText="Environ 160 caractères"
-                              renderWidget={(className) => (
-                                <>
-                                  <textarea id="seo.description" className={className} />
-                                </>
-                              )}
-                            />
-                          </fieldset>
-                        )}
-                      </Disclosure.Panel>
-                    </>
-                  )}
-                </Disclosure>
               </Disclosure.Panel>
             </>
           )}
         </Disclosure>
       </div>
-
-      <div className="">{SubmitButton}</div>
     </form>
   );
 }
