1cdf972ca2bbd0c05037179ad04eb618939903a8
odinalx <odin.alexandre26@gmail.com>
2024-06-12 14:54:32 +0200

    feature: add color indicator of how much errors article or stock have

--- apps/storefront/app/admin/seo/ArticlesForms.tsx
+++ apps/storefront/app/admin/seo/ArticlesForms.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 'use client';
 
 import { Article } from '@couture-next/types';
@@ -15,18 +16,44 @@ export default function ArticlesForms() {
   return (
     <Tab.Group as="div" className="grid sm:grid-cols-[auto_1fr] gap-4">
       <Tab.List className="grid grid-cols-[repeat(auto-fill,4rem)] place-content-start py-2 gap-2 sm:max-h-[calc(100dvh-9rem)] px-4 overflow-y-auto h-full border w-full min-w-80 max-h-40 sm:max-w-sm">
-        {articlesQuery.data?.map((article) => (
-          <Tab className="aspect-square !outline-none">
-            <Image
-              src={article.images[0].url}
-              loader={loader}
-              width={64}
-              height={64}
-              alt={article.name}
-              className="w-16 aspect-square object-contain object-center ui-selected:outline outline-primary-100"
-            />
-          </Tab>
-        ))}
+        {articlesQuery.data?.map((article) => {
+          let errors = 0;
+          if (article.seo.description.length < 90 || article.seo.description.length > 150) {
+            errors++;
+          }
+          if (article.seo.title.length < 44 || article.seo.title.length > 54) {
+            errors++;
+          }
+          article.stocks.forEach((stock) => {
+            if (stock.seo.description.length < 90 || stock.seo.description.length > 150) {
+              errors++;
+            }
+            if (stock.seo.title.length < 44 || stock.seo.title.length > 54) {
+              errors++;
+            }
+          });
+          return (
+            <Tab className="aspect-square !outline-none relative">
+              <Image
+                src={article.images[0].url}
+                loader={loader}
+                width={64}
+                height={64}
+                alt={article.name}
+                className="w-16 aspect-square object-contain object-center ui-selected:outline outline-primary-100"
+              />
+              {errors >= 4 && (
+                <div className="rounded-full bg-red-600 w-4 p-2 absolute bottom-0.5 right-0.5 border border-black "></div>
+              )}
+              {errors >= 1 && errors <= 3 && (
+                <div className="rounded-full bg-orange-600 w-4 p-2 absolute bottom-0.5 right-0.5 border border-black "></div>
+              )}
+              {errors === 0 && (
+                <div className="rounded-full bg-green-600 w-4 p-2 absolute bottom-0.5 right-0.5 border border-black "></div>
+              )}
+            </Tab>
+          );
+        })}
       </Tab.List>
       <Tab.Panels>
         {articlesQuery.data?.map((article) => (
@@ -48,18 +75,37 @@ function InStockArticlesTabs({ article }: { article: Article }) {
   return (
     <Tab.Group as="div" className="grid sm:grid-cols-[auto_1fr] gap-4 sm:h-full">
       <Tab.List className="grid grid-cols-[repeat(auto-fill,4rem)] place-content-start min-w-80 justify-center py-2 gap-2 px-4 overflow-y-auto sm:h-full border sm:max-w-sm w-full h-40 sm:max-h-auto">
-        {article.stocks.map((stock) => (
-          <Tab className="!outline-none">
-            <Image
-              src={stock.images[0].url}
-              loader={loader}
-              width={64}
-              height={64}
-              alt={stock.title}
-              className="w-16 aspect-square object-contain object-center ui-selected:outline outline-primary-100 ui-not-selected:outline-none"
-            />
-          </Tab>
-        ))}
+        {article.stocks.map((stock) => {
+          let errors = 0;
+          if (stock.seo.description.length < 90 || stock.seo.description.length > 150) {
+            errors++;
+          }
+          if (stock.seo.title.length < 44 || stock.seo.title.length > 54) {
+            errors++;
+          }
+          const dotcolor = errors >= 4 ? 'red' : errors >= 1 && errors <= 3 ? 'orange' : 'green';
+          return (
+            <Tab className="!outline-none relative">
+              <Image
+                src={stock.images[0].url}
+                loader={loader}
+                width={64}
+                height={64}
+                alt={stock.title}
+                className="w-16 aspect-square object-contain object-center ui-selected:outline outline-primary-100 ui-not-selected:outline-none"
+              />
+              {errors >= 4 && (
+                <div className="rounded-full bg-red-600 w-4 p-2 absolute bottom-0.5 right-0.5 border border-black "></div>
+              )}
+              {errors >= 1 && errors <= 3 && (
+                <div className="rounded-full bg-orange-600 w-4 p-2 absolute bottom-0.5 right-0.5 border border-black "></div>
+              )}
+              {errors === 0 && (
+                <div className="rounded-full bg-green-600 w-4 p-2 absolute bottom-0.5 right-0.5 border border-black "></div>
+              )}
+            </Tab>
+          );
+        })}
       </Tab.List>
       <Tab.Panels className="sm:h-full">
         {article.stocks.map((stock, i) => (
