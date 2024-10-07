bafbfcf46a3439c8c607ac21df801706ae4abb0d
Thomas Bouillon <tbouillon54@gmail.com>
2024-10-07 22:55:09 +0200

    fix: trigger isr

--- libs/api-connector/src/lib/routers/articles/updatePositions.ts
+++ libs/api-connector/src/lib/routers/articles/updatePositions.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 import { publicProcedure } from '../../trpc';
 import { isAdmin } from '../../middlewares/isAdmin';
+import { triggerISR } from '../../isr';
 
 const schema = z.array(z.string().min(1)).transform((value) => {
   return Array.from(new Set(value));
@@ -15,11 +16,33 @@ export default publicProcedure
       position: index,
     }));
 
+    const prev = await ctx.orm.article.findMany({
+      where: { id: { in: input } },
+      select: { id: true, position: true, slug: true },
+    });
+
+    const edited = prev.reduce((acc, article) => {
+      const newPosition = positions.find((position) => position.id === article.id)?.position;
+      if (newPosition !== undefined && newPosition !== article.position) {
+        acc.push({
+          id: article.id,
+          position: newPosition,
+          slug: article.slug,
+        });
+      }
+      return acc;
+    }, [] as { id: string; position: number; slug: string }[]);
+
     await Promise.all(
-      positions.map(({ id, position }) => {
-        return ctx.orm.article.update({
+      edited.map(async ({ id, position, slug }) => {
+        await ctx.orm.article.update({
           where: { id },
           data: { position },
+        });
+        await triggerISR(ctx, {
+          resource: 'articles',
+          event: 'update',
+          article: { id, slug },
         });
       })
     );
